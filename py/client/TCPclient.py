10b4bbf40d7f066bbaecaf489346c0e429f8c322
gurbaflurb <seachen4411@gmail.com>
2020-04-15 18:05:25 -0600

    Finalized TCP for python

--- py/client/TCPclient.py
+++ py/client/TCPclient.py
@@ -2,31 +2,35 @@
 import socket
 import sys
 
-port = 2248
+port = 2248 # Specify the port for the client
 
-if(len(sys.argv) < 3):
+if(len(sys.argv) < 3): # Check to make sure enough arguments were supplied
     print(f'Usage: {sys.argv[0]} server-addr file-name')
     exit()
 addr = str(sys.argv[1])
 transferFile = str(sys.argv[2])
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a client socket and connect to the server provided
 client.connect((addr, port))
 
-client.send(transferFile.encode('utf-8'))
+client.send(transferFile.encode('utf-8')) # Send the server the name of the file requested
 
-response = client.recv(1024).decode()
-if(response == 'File not found'):
+response = client.recv(1024).decode() # Get servers response
+if(response == 'File not found'): # If the file was not found, print as such and quit
     print('Server failed to find file!')
     client.close()
     exit()
+else:
+    print(response) # Else print the servers response
 
-file = open(transferFile, 'wb')
-while(True):
-    data = client.recv(1024)
-    while(True):
-        file.write(data)
-        data = client.recv(1024)
+file = open(transferFile, 'wb') # Open a file to write as binary
+data = client.recv(1024) # Receive the first 1024 bytes of data from server
+while(True): # Infinite loop for the server to send data
+    file.write(data) # Write the sent data to the file opened
+    data = client.recv(1024) # Receive the next 1024 bytes of data
+    if(data == b''): # Check if the server is sending null strings
+        print('File downloaded!') # If so print the server is done and break out of the loop
+        break
 
-file.close()
+file.close() # Close the file and the connection
 client.close()
