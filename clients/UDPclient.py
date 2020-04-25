8f20239b1b624f4a9ffc174e71dcdd29a814cb85
gurbaflurb <seachen4411@gmail.com>
2020-04-24 19:38:33 -0600

    Added sequence numbers to packets and finished UDP

--- clients/UDPclient.py
+++ clients/UDPclient.py
@@ -1,56 +1,58 @@
 #!/usr/bin/python3
 import socket
 import sys
-from hashlib import md5
+import pickle
 
-def writeFile(client, transferFile):
-    file = open(transferFile, 'wb')
-    data = client.recv(1024)
-    while(True):
-        file.write(data)
-        data = client.recv(1024)
-        if(data == b''):
-            break
+def writeFile(fileName, fileData, totalPackets):
+    assert isinstance(fileName, str)
+    assert isinstance(fileData, dict)
+    assert isinstance(totalPackets, int)
 
-    file.close()
-
-def getNewFileHash(fileName):
-    hasher = md5()
-    with open(fileName, 'rb') as file:
-        hasher.update(file.read())
-    return hasher.hexdigest()
-
+    with open(fileName, 'wb') as file:
+        for i in range(1, totalPackets+1):
+            file.write(fileData[i])
 
 def main():
-    port = 2248
-
     if(len(sys.argv) < 3):
         print(f'Usage: {sys.argv[0]} server-addr file-name\n')
         exit()
+    
+    port = 2248
     addr = socket.gethostbyname(sys.argv[1])
-    transferFile = str(sys.argv[2])
+    fileRequested = sys.argv[2]
+    is_buffer_filled = False
 
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client.connect((addr, port))
+    client.send(fileRequested.encode('utf-8'))
 
-    client.send(transferFile.encode('utf-8'))
-
-    fileHash = client.recv(1024).decode()
-    if(fileHash == 'File not found'):
+    response = client.recv(2048).decode()
+    if(response == 'File not found'):
         print('Server failed to find file!')
         client.close()
         exit()
     else:
-        print(f'File found! MD5 Hash:{fileHash}')
-        writeFile(client, transferFile)
-        currentHash = getNewFileHash(transferFile)
-            
-        while(fileHash != currentHash):
-            client.send(transferFile.encode('utf-8'))
-            client.recv(1024)
-            writeFile(client, transferFile)
-            currentHash = getNewFileHash(transferFile)
-        print('File downloaded!')
+        print(f'Server found file, packets to be recieved: {response}')
+        fileData = {}
+        while not is_buffer_filled:
+            data = client.recv(2048)
+            if(data == b''):
+                is_buffer_filled = True
+                for i in range(1, int(response)+1):
+                    if (i not in fileData):
+                        print(f'Missing Packet(s) {i}')
+                        is_buffer_filled = False
+                if(not is_buffer_filled):
+                    print('Re-requesting file and saving what we got!')
+                    client.send(fileRequested.encode('utf-8'))
+                    response = client.recv(2048).decode()
+                else:
+                    break
+            else:
+                tempDict = pickle.loads(data)
+                fileData[tempDict['seq']] = tempDict['data']
+        writeFile(fileRequested, fileData, int(response))
+        
     client.close()
 
 if __name__ == "__main__":
